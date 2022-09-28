4d59d7b464e5bfdde64b6f2e115a5c185859d826
Radu Ciocan <ciocan@gmail.com>
2022-09-28 09:12:12 +0300

    add cors

--- pages/api/is-blocked.ts
+++ pages/api/is-blocked.ts
@@ -1,3 +1,4 @@
+import Cors from "cors";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 interface Error {
@@ -10,7 +11,25 @@ interface Ok {
   forwarded: string | string[] | undefined;
 }
 
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
+
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Ok | Error>) {
+  await runMiddleware(req, res, cors);
+
   try {
     const ip = req.socket.remoteAddress;
     const forwarded = req.headers["x-forwarded-for"];
