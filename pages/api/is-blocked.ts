615d26534118147470a080863bde50ec7d8f3e18
Radu Ciocan <ciocan@gmail.com>
2022-09-28 08:58:52 +0300

    add forwarded

--- pages/api/is-blocked.ts
+++ pages/api/is-blocked.ts
@@ -7,15 +7,17 @@ interface Error {
 interface Ok {
   ok: boolean;
   ip: string | undefined;
+  forwarded: string | string[] | undefined;
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Ok | Error>) {
   try {
     const ip = req.socket.remoteAddress;
+    const forwarded = req.headers["x-forwarded-for"];
 
-    console.log(ip);
+    console.log(ip, forwarded);
 
-    return res.status(200).json({ ok: true, ip });
+    return res.status(200).json({ ok: true, ip, forwarded });
   } catch (e) {
     console.error(e);
     return res.status(500).json({ error: String(e) });
