ee282d31fe6c6a65837a4678bc81209cf9c777af
Radu Ciocan <ciocan@gmail.com>
2022-10-07 18:54:46 +0100

    add blocker

--- pages/api/is-blocked.ts
+++ pages/api/is-blocked.ts
@@ -1,6 +1,23 @@
 import Cors from "cors";
 import type { NextApiRequest, NextApiResponse } from "next";
 
+const BLOCKED_COUNTRIES = [
+  "CU", // Cuba
+  "CF", // Central African Republic
+  "IR", // Iran
+  "IQ", // Iraq
+  "LB", // Lebanon
+  "LY", // Libya
+  "KP", // North Korea
+  "RU", // Russia
+  "SO", // Somalia
+  "SS", // South Sudan
+  "SD", // Sudan and Darfur
+  "SY", // Syria
+  "VE", // Venezuela
+  "YE", // Yemen
+];
+
 interface Error {
   error: string;
 }
@@ -28,7 +45,7 @@ function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Ok | Error>) {
   await runMiddleware(req, res, cors);
-  return res.status(200).json({ blocked: false });
+  // return res.status(200).json({ blocked: false });
 
   try {
     const ip = req.headers["x-forwarded-for"];
@@ -41,10 +58,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
     const {
       security: { vpn, proxy, tor, relay },
-      location: { country_code: country },
+      location: { country_code: countryCode },
     } = ipInfo;
 
-    const isBocked = vpn || proxy || tor || relay || country === "US";
+    const isBocked = vpn || proxy || tor || relay || BLOCKED_COUNTRIES.includes(countryCode);
 
     return res.status(200).json({ blocked: isBocked, ip });
   } catch (e) {
