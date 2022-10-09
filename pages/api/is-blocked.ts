243d380c15f39cc73b087c13e12af8264179d663
Radu Ciocan <ciocan@gmail.com>
2022-10-09 13:20:37 +0100

    fix redis read

--- pages/api/is-blocked.ts
+++ pages/api/is-blocked.ts
@@ -58,7 +58,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     const value = await client.get(ip);
 
     if (value !== null) {
-      return res.status(200).json({ blocked: !!value, ip });
+      return res.status(200).json({ blocked: !!Number(value), ip });
     }
 
     const response = await fetch(`https://vpnapi.io/api/${ip}?key=${process.env.VPNAPI_KEY}`);
